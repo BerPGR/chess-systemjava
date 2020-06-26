e07dd683442809f32a756fcf81f54d537086de26
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2020-06-26 07:56:26 -0300

    Chess project with advanced Chessboard

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Locale;
+
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+
+	}
+
+}
